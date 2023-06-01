3f9fd8a7309cbf578ef7b0aea8d124fca6670661
Olive <115409596+Grit1021@users.noreply.github.com>
2023-06-01 17:10:56 +0800

    Add files via upload

---
+++ task1/test.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.utils as vutils
+import torchvision
+import torchvision.transforms as transforms
+import tqdm
+from models.resnet import *
+from tqdm import trange
+from train_models import train,acc_on_test
+
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2675, 0.2565, 0.2761)),
+])
+trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=512, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False, num_workers=2)
+'''
+
+{19: 'cattle', 29: 'dinosaur', 0: 'apple', 11: 'boy', 1: 'aquarium_fish', 86: 'telephone', 90: 'train', 28: 'cup', 23: 'cloud', 31: 'elephant', 39: 'keyboard', 96: 'willow_tree', 82: 'sunflower', 17: 'castle', 71: 'sea', 8: 'bicycle', 97: 'wolf', 80: 'squirrel', 74: 'shrew', 59: 'pine_tree', 70: 'rose', 87: 'television', 84: 'table', 64: 'possum', 52: 'oak_tree', 42: 'leopard', 47: 'maple_tree', 65: 'rabbit', 21: 'chimpanzee', 22: 'clock', 81: 'streetcar', 24: 'cockroach', 78: 'snake', 45: 'lobster', 49: 'mountain', 56: 'palm_tree', 76: 'skyscraper', 89: 'tractor', 73: 'shark', 14: 'butterfly', 9: 'bottle', 6: 'bee', 20: 'chair', 98: 'woman', 36: 'hamster', 55: 'otter', 72: 'seal', 43: 'lion', 51: 'mushroom', 35: 'girl', 83: 'sweet_pepper', 33: 'forest', 27: 'crocodile', 53: 'orange', 92: 'tulip', 50: 'mouse', 15: 'camel', 18: 'caterpillar', 46: 'man', 75: 'skunk', 38: 'kangaroo', 66: 'raccoon', 77: 'snail', 69: 'rocket', 95: 'whale', 99: 'worm', 93: 'turtle', 4: 'beaver', 61: 'plate', 94: 'wardrobe', 68: 'road', 34: 'fox', 32: 'flatfish', 88: 'tiger', 67: 'ray', 30: 'dolphin', 62: 'poppy', 63: 'porcupine', 40: 'lamp', 26: 'crab', 48: 'motorcycle', 79: 'spider', 85: 'tank', 54: 'orchid', 44: 'lizard', 7: 'beetle', 12: 'bridge', 2: 'baby', 41: 'lawn_mower', 37: 'house', 13: 'bus', 25: 'couch', 10: 'bowl', 57: 'pear', 5: 'bed', 60: 'plain', 91: 'trout', 3: 'bear', 58: 'pickup_truck', 16: 'can'}
+'''
+
+print("The Number of Samples in Train Set is :",len(trainset))
+print("The Number of Samples in Test Set is :",len(testset))
+
+model = resnet.resnet18(num_classes=100).cuda()
+model = torch.load('./models/datas/baseline_aug_data.pth')
+
+print(acc_on_test(model,testloader))
---
+++ task1/train.py
@@ -0,0 +1,54 @@
+from cgi import test
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.utils as vutils
+import torchvision
+import torchvision.transforms as transforms
+import tqdm
+from models import resnet
+from tqdm import trange
+from models.train_models import train,acc_on_test,dic #可更换不同的train函数
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+
+
+
+
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),  #先四周填充0，图像随机裁剪成32*32
+    transforms.RandomHorizontalFlip(),  #图像一半的概率翻转，一半的概率不翻转
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2675, 0.2565, 0.2761)), #R,G,B每层的归一化用到的均值和方差
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2675, 0.2565, 0.2761)),
+])
+trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=512, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False, num_workers=2)
+
+lr = 0.1
+model = resnet.resnet18(num_classes=100).cuda()
+optimizer = optim.SGD(model.parameters(),lr=lr,momentum=0.9,weight_decay=5e-4)
+criterion = nn.CrossEntropyLoss()
+
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, [80,120,150], gamma=0.1)
+
+writer = SummaryWriter('./log')
+
+
+epoch = 200
+batchsize = 512
+loss,val_acc, test_acc = train_mixup(model,optimizer,criterion,batchsize, epoch,trainset,testloader,scheduler)
+torch.save(model,'./models/output/baseline_v2.pth')
+data = {'loss':loss,'val_acc':val_acc,'test_acc':test_acc}
+torch.save(data,'./models/datas/baseline_v2_data.pth')
+
